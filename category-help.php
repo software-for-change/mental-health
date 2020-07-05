2fd5698c28c227efb792d1f78449c8df1df86c28
Nekesa Mercy <kesamercy@gmail.com>
2020-07-04 20:35:07 -0400

    made ui changes to fit mobile version

--- category-help.php
+++ category-help.php
@@ -21,12 +21,12 @@ include_once "access-db.php";
 </head>
 
 <body>
-    <div class="w3-center sidebar">
+    <!-- <div class="w3-center sidebar">
         <a class="active" href="home.php">Home</a>
         <a href="volunteer.html">Volunteer</a>
         <a href="recommend.php">recommend</a>
         <a href="about.html">About</a>
-    </div>
+    </div> -->
 
     <div class="content category-content">
 
