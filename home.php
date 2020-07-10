d065e00e8af69ed8f73c1cda7ee9612ce7346fc9
Nekesa Mercy <kesamercy@gmail.com>
2020-07-10 10:15:30 -0400

    added the meta tag required for media queries on a page

--- home.php
+++ home.php
@@ -9,9 +9,8 @@ include_once "access-db.php";
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Mental Health</title>
-
-
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
