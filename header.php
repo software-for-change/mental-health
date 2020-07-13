55d8339285fc309961f8cd4a597b9cc6819a685c
Nekesa Mercy <kesamercy@gmail.com>
2020-07-13 14:55:25 -0400

    added more naviagation links to the mobile and the desktop navs

--- header.php
+++ header.php
@@ -6,17 +6,20 @@ echo "
     <ul class='topnav'>
                 <li><a class='logo' href='#home'>Journey to Mental Wealth</a></li>
 
+                <li class='right'><a href='category-validate.php'>Signs</a></li>
+                <li class='right'><a href='category-help.php'>Help</a></li>
                 <li class='right'><a href='recommend.php'>Recommend a Resource</a></li>
-                <li class='right'><a href='about.html'>About</a></li>
+                <li class='right'><a href='about.php'>About</a></li>
             </ul>
 
             <!-- mobile navigation -->
             <div id='mySidepanel' class='sidepanel'>
                 <a href='javascript:void(0)' class='closebtn' onclick='closeNav()'>×</a>
-                <a href='#'>About</a>
-                <a href='#'>Services</a>
-                <a href='#'>Clients</a>
-                <a href='#'>Contact</a>
+                <a href='home.php'>Home</a>
+                <a href='category-validate.php'>Signs</a>
+                <a href='category-help.php'>Help</a>
+                <a href='about.php'>About</a>
+                <a href='recommend.php'>Recommend a Resource</a>
             </div>
 
             <button class='openbtn mobile-nav' onclick='openNav()'>☰ Menu </button>
