9b22b94c9b9d65b121dd20f0c83ff8b13e381a10
Nekesa Mercy <kesamercy@gmail.com>
2020-07-13 15:00:30 -0400

    chanhing color for active link

--- about.php
+++ about.php
@@ -11,9 +11,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Mental Health</title>
 
-
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" type="text/css" href="style.css" />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700&display=swap" rel="stylesheet">
