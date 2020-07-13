e15e0045b6c572d0b5d38ddc8037bcdd728f4ff4
Nekesa Mercy <kesamercy@gmail.com>
2020-07-13 13:26:42 -0400

    added js code for controling the tab links for the validate and help content

---
+++ index.js
@@ -0,0 +1,17 @@
+// this is the code for the tabs that show the contnet on the help and naviagte pages
+function openPage(pageName, elmnt, color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
