b3a1b104d34a05ac3f9542037fef83882bc85845
Nekesa Mercy <kesamercy@gmail.com>
2020-07-13 20:22:49 -0400

    added feedback form on footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "     <footer>
+echo "    <footer>
     
     <div class='w3-row'>
 
@@ -16,6 +16,16 @@ echo "     <footer>
 
     <div class='w3-third w3-center'>
         <p>Contact Us</p>
+        <button onclick='document.getElementById('id01').style.display='block'' class='w3-button w3-black'>Feedback</button>
+        <div id='id01' class='w3-modal'>
+        <div class='w3-modal-content'>
+          <div class='w3-container'>
+            <span onclick='document.getElementById('id01').style.display='none'' class='w3-button w3-display-topright'>&times;</span>
+            <p>Some text. Some text. Some text.</p>
+            <p>Some text. Some text. Some text.</p>
+          </div>
+        </div>
+      </div>
     </div>
 
 </div>
