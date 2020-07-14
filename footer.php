e8f769fea383f10001a280785ff9d0b35b6fe313
Nekesa Mercy <kesamercy@gmail.com>
2020-07-13 20:45:29 -0400

    added feedback form on footer- pop up model debugging

--- footer.php
+++ footer.php
@@ -17,11 +17,11 @@ echo "    <footer>
     <div class='w3-third w3-center'>
         <p>Contact Us</p>
         <div class='w3-container'>
-        <button onclick='document.getElementById('id01').style.display='block'' class='w3-button w3-black'>Feedback</button>
+        <button onclick=\"document.getElementById(\'id01\').style.display='block'\" class='w3-button w3-black'>Feedback</button>
         <div id='id01' class='w3-modal'>
         <div class='w3-modal-content'>
           <div class='w3-container'>
-            <span onclick='document.getElementById('id01').style.display='none'' class='w3-button w3-display-topright'>&times;</span>
+            <span onclick=\"document.getElementById(\'id01\').style.display='none'\" class='w3-button w3-display-topright'>&times;</span>
             <p>Some text. Some text. Some text.</p>
             <p>Some text. Some text. Some text.</p>
           </div>
