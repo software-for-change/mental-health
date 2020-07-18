0e7b254292b3c7a7e8908ae277fcf64f2051df81
Nekesa Mercy <kesamercy@gmail.com>
2020-07-17 21:22:47 -0400

    adding back browse icons

--- category-validate.php
+++ category-validate.php
@@ -47,13 +47,7 @@ include_once "access-db.php";
                         <input class="email-input" type="text" placeholder="Enter your email"> 
                         <input class="email-submit category-submit-btn" type="submit" value="Never miss an article">
                     </form>
-                    <a href="">Browse categories</a>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-back" width="44"
-                        height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#9E9E9E" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" />
-                        <path d="M9 11l-4 4l4 4m-4 -4h11a4 4 0 0 0 0 -8h-1" />
-                    </svg>
+                    <a> <i style='font-size:20px' class='fas'>&#xf060;</i> Browse Categories</a>
                 </div>
 
             </div>
